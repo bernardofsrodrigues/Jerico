576880229a515343cb2241150e415213b3861888
Bernardo <rodriguesfsbernardo@gmail.com>
2023-12-15 11:41:46 -0300

    terminei pag login, register e alterar senha

--- API's/alterar_senha.js
+++ API's/alterar_senha.js
@@ -1,6 +1,6 @@
 /* 
-const email = 'emailteste@gmail.com';
-const senha = 'maa123435a';                 //parametros de teste
+const email = 'teste@gmail.com';
+const senha = 'teste';                 //parametros de teste
 const nova_senha = 'fsgs12313141g';
 
 const email = document.getElementById('email').value; //colocar IDs ainda
@@ -14,7 +14,7 @@ const nova_senha = $('#novasenha').val();
 
 
 async function alterarSenha() {
-    const url = 'https://consultconsig.azurewebsites.net/api/identidade/alterar-senha';
+    const url = 'http://carlosjunior007-001-site1.etempurl.com/api/identidade/alterar-senha';
 
     const data = {
         email: email,
@@ -37,3 +37,6 @@ async function alterarSenha() {
 }
 
 //200
+
+/*{"type":"https://tools.ietf.org/html/rfc7231#section-6.5.1","title":"One or more validation errors occurred.","status":400,"traceId":"00-87b9a6054a38fa3e12183360f6a6782a-cf90aaa3182549b8-00","errors":{"SenhaAtual":["O campo SenhaAtual precisa ter entre 6 e 100 caracteres"]}}
+400*/
